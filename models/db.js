610b5a70288062f5ec713c20ae63fdccefeef207
NguyenDucDungMc <dungndph26258@fpt.edu.vn>
2023-10-04 14:11:03 +0700

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://luyenvtph24062:luyenthanhvu123@cluster0.xfuogwv.mongodb.net/' + 'DuAnTotNghiep' + '?retryWrites=true&w=majority').catch((err) => {
+mongoose.connect('mongodb+srv://luyenvtph24062:luyenthanhvu123@cluster0.xfuogwv.mongodb.net/' + 'DuAnTotNghiep' + '?retryWrites=true&w=majority')
+.catch((err) => {
     console.log("Lỗi kết nối CSDL");
     console.log(err);
 });
