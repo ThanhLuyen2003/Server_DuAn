d56ca2426789c2fba319d13907ccac54473eeeb3
huynqph23996 <huynqph23996@fpt.edu.vn>
2023-10-15 16:05:39 +0700

    Update comment.apiRoute.js

--- routes/comment.apiRoute.js
+++ routes/comment.apiRoute.js
@@ -3,6 +3,6 @@ var router = express.Router();
 
 var comment_api= require('../api/Comment.api');
 
-router.get('/comment',comment_api.Comment);
+router.get('/comment/:idPosts',comment_api.Comment);
 
 module.exports = router;
