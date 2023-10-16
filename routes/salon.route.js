6b439ed9b0e4fae1a828105b44f2388b3c523ca0
NguyenDucDungMc <dungndph26258@fpt.edu.vn>
2023-10-16 13:51:51 +0700

    edit

--- routes/salon.route.js
+++ routes/salon.route.js
@@ -11,4 +11,10 @@ router.get('/', SalonController.list);
 router.get('/add', SalonController.addSalon);
 router.post('/add',uploader.single('file_anh'), SalonController.addSalon);
 
+router.get('/edit/:ids', SalonController.editSalon);
+router.post('/edit/:ids',uploader.single('file_anh'), SalonController.editSalon);
+
+router.get('/delete/:ids', SalonController.deleteSalon);
+router.post('/delete/:ids', SalonController.deleteSalon);
+
 module.exports = router;
