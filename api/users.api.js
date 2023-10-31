6910633a7649a91ba153357618832212c1d17953
huynqph23996 <huynqph23996@fpt.edu.vn>
2023-10-31 22:42:17 +0700

    Update users.api.js

--- api/users.api.js
+++ api/users.api.js
@@ -1,6 +1,6 @@
 var md = require('../models/model');
 
- exports.users = async(req, res, next)=>{
+exports.users = async (req, res, next) => {
     try {
         let id = await md.userModel.find();
 
@@ -19,3 +19,21 @@ var md = require('../models/model');
     }
 }
 
+
+
+exports.updateUser = async (req, res, next) => {
+    try {
+        const userId = req.params.id;
+        const updatedUserInfo = req.body;
+        const updatedUser = await md.userModel.findByIdAndUpdate(userId, updatedUserInfo, { new: true });
+        if (updatedUser) {
+            res.status(200).json(updatedUser);
+        } else {
+            res.status(400).json({ msg: 'user not foud' });
+        }
+    } catch (error) {
+        return res.status(500).json({
+            msg: err.message
+        });
+    }
+}
