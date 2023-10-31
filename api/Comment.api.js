b36f96b1d8678d311c30479ae2ec0571c853b4de
huynqph23996 <huynqph23996@fpt.edu.vn>
2023-10-31 14:23:48 +0700

    Update Comment.api.js

--- api/Comment.api.js
+++ api/Comment.api.js
@@ -1,15 +1,53 @@
 var md = require('../models/model');
 
-exports.Comment = async (req, res, next) => {
-        const idu= req.params.idPosts
-        try {
-            const story= await md.productModel.findById(idu)
-            if(!story){
-                return res.status(404).json({messge:"Bai viet khong ton tai"})
-            }
-            
-            res.status(200).json({story})
-        } catch (error) {
-            return res.status(500).json({messge:error.messge})
+exports.getComment = async (req, res, next) => {
+    try {
+        const idPosts = req.params.idPosts;
+
+        if (!idPosts) {
+            return res.status(400).json({ message: 'Invalid post ID' });
         }
-}
+
+        const comments = await md.CommentModel.find({ idPosts });
+
+        if (comments) {
+            res.status(200).json(comments);
+        } else {
+            res.status(204).json({
+                msg: 'khong co du lieu',
+
+            });
+        }
+    } catch (error) {
+        
+        res.status(500).json({ error: error.message });
+    }
+};
+
+
+
+exports.addComment =async(req,res,next)=>{
+    try {
+        const { idUser, idPosts } = req.params;
+        const { Comment,avatarUser,nameUser } = req.body;
+
+        const newComment = new md.CommentModel({
+            Comment,
+            avatarUser,
+            idUser,
+            idPosts, // Assuming idPosts refers to idProductSalon
+            nameUser
+        });
+
+        // Save the new comment   
+await newComment.save();
+
+        res.status(200).json({ message: 'Comment added successfully' });
+    } catch (err) {
+        
+       
+return res.status(500).json({ message: err.message });
+    }
+};
+
+
