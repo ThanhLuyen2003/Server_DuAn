3ea0013fa54f6b55e5d81d594e3d02315e52f9b6
huynqph23996 <huynqph23996@fpt.edu.vn>
2023-10-31 16:13:35 +0700

    Update API.js

--- api/API.js
+++ api/API.js
@@ -76,18 +76,30 @@ app.post('/addBillDetail', async (req, res) => {
 
 
 app.post('/addUser', async (req, res) => {
+    const { name, email, phone, pass, avatar, address } = req.body;
 
-    var u = new md.userModel(req.body);
-
-    try {
-        await u.save();
-
-        res.status(200).json(u);
-
+    try {  
+        const phonee = await md.userModel.findOne({ phone: phone });
+        const emaill = await md.userModel.findOne({ email: email });
+        if (phonee) {
+            return res.status(400).json({ error: 'Số điện thoại hoặc email đã tồn tại' });
+        }else if(emaill){
+            return res.status(400).json({ error: 'Số điện thoại hoặc email đã tồn tại' });
+        }
+        const newUser = new md.userModel({
+            name,
+            email,
+            phone,
+            pass,
+            avatar,
+            address
+        });
+await newUser.save();
+        res.status(200).json({ message: 'Người dùng được thêm thành công' });
     } catch (error) {
-        res.status(500).send(error);
+        res.status(500).json({ error: error.message });
     }
-})
+});
 
 
 app.get('/getBill/:idUser', (req, res) => {
