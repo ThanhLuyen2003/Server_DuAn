3fdb5d5a49f153f56d445de72e27bf04b41393c3
huynqph23996 <huynqph23996@fpt.edu.vn>
2023-11-03 16:09:52 +0700

    Update ProductSalon.apiRoute.js

--- routes/ProductSalon.apiRoute.js
+++ routes/ProductSalon.apiRoute.js
@@ -5,4 +5,16 @@ var users_api = require('../api/ProductSalon.api');
 
 router.get('/productsalon', users_api.productsalon);
 
+//SAP
+router.get('/productsalon/Sap',users_api.productsalonSap);
+//Cham soc da
+router.get('/productsalon/ChamSocDa',users_api.productsalonChamSocDa);
+//Cham soc co the
+router.get('/productsalon/ChamSocCoThe',users_api.productsalonChamSocCoThe);
+//Combo
+router.get('/productsalon/Combo',users_api.productsalonCombo);
+//Nuoc hoa
+router.get('/productsalon/NuocHoa',users_api.productsalonNuocHoa);
+//Kem danh rang
+router.get('/productsalon/KemDanhRang',users_api.productsalonKemDanhRang);
 module.exports = router;
