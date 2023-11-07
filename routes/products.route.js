f536ef192f2ed232f9b4cb329899e6ba87a94221
NguyenDucDungMc <dungndph26258@fpt.edu.vn>
2023-11-07 11:28:26 +0700

    Create products.route.js

---
+++ routes/products.route.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var multer = require('multer');
+var uploader = multer( { dest: './tmp'} );
+
+var ProductController = require('../controllers/productscontroller');
+
+router.get('/', ProductController.list);
+
+
+router.get('/add', ProductController.addPro);
+router.post('/add',uploader.single('file_anh'), ProductController.addPro);
+
+router.get('/edit/:idp', ProductController.editPro);
+router.post('/edit/:idp',uploader.single('file_anh'), ProductController.editPro);
+
+router.get('/delete/:idp', ProductController.deletePro);
+router.post('/delete/:idp', ProductController.deletePro);
+
+
+module.exports = router;
