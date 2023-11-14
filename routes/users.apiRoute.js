585fb70824396eec65680b4eda66a74d9ce67cca
huynqph23996 <huynqph23996@fpt.edu.vn>
2023-11-14 14:18:41 +0700

    Update users.apiRoute.js

--- routes/users.apiRoute.js
+++ routes/users.apiRoute.js
@@ -7,5 +7,10 @@ router.get('/users',users_api.users);
 
 router.put('/updateUsers/:id', users_api.updateUser); // Route to update a user by ID
 
+router.put('/updateAvatar/:id', users_api.updateAvatar); // Route to update a user by ID
+
 router.put('/updateOTP/:id',users_api.OTPGiaoDich);
+
+router.delete('/deleteUser/:id', users_api.deleteUser);
+
 module.exports = router;
