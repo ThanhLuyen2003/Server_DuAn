cebb4d6bb92cf396eb5b884ee9d6c435578ad6e7
NguyenDucDungMc <dungndph26258@fpt.edu.vn>
2023-11-19 00:13:50 +0700

    1

--- routes/home.route.js
+++ routes/home.route.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var homeController = require('../controllers/homecontroller');
 var check_login = require('../middlewares/check_login');
 
-router.get('/hom',check_login.yeu_cau_dang_nhap,homeController.home);
-router.post('/hom',check_login.yeu_cau_dang_nhap,homeController.home);
+router.get('/',check_login.yeu_cau_dang_nhap,homeController.home);
+router.post('/',check_login.yeu_cau_dang_nhap,homeController.home);
 
 module.exports = router;
