348266ceb8ed104b3f5fa376575e3550d64dc652
Dhung227 <117275513+Dhung227@users.noreply.github.com>
2023-11-23 16:24:27 +0700

    bam xac nhan hoan thanh lich dat

--- controllers/homecontroller.js
+++ controllers/homecontroller.js
@@ -1,4 +1,4 @@
-var Bill = require('../models/BillModel');
+var billDB = require('../models/BillModel');
 var fs = require ('fs');
 exports.home = async(req,res,next) =>{
     const page = parseInt(req.query.page) || 1;
@@ -9,8 +9,8 @@ exports.home = async(req,res,next) =>{
     try {
         const sortOptions = {};
     sortOptions[sortBy] = sortOrder === 'desc' ? -1 : 1;
-        var listBill = await Bill.find().skip(skip).limit(limit).sort(sortOptions).populate('idUser');
-        var totalBill = await Bill.countDocuments();
+        var listBill = await billDB.find().skip(skip).limit(limit).sort(sortOptions).populate('idUser');
+        var totalBill = await billDB.countDocuments();
     
       } catch (err) {
         console.error('Error retrieving users:', err);
@@ -19,4 +19,26 @@ exports.home = async(req,res,next) =>{
     res.render('home/danhsach',{listBill: listBill, currentPage: page,
         totalPages: Math.ceil(totalBill / limit),
         totalBill});
+}
+
+exports.xac_nhan_lich_dat = async(req, res, next) => {
+  console.log('Xác nhận lịch đặt');
+  let ids = req.params.ids;
+  // console.log(await bill.findById(ids));
+  console.log(ids+'aaaaaaa');
+  let objBill = await billDB.findById(ids);
+
+  console.log('BEFORE'+objBill.status);
+  objBill.status = 'Đã hoàn thành';
+  objBill._id = ids;
+
+  try {
+    console.log('AFTER'+objBill.status);
+    await billDB.findByIdAndUpdate({ _id: ids }, objBill);
+    console.log('AFTER UPDATE'+objBill.status);
+  } catch (error) {
+    console.log(error);
+  }
+
+  res.redirect('/home');
 }
