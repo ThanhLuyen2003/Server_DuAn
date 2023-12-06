b8eed33e1d7529d9578957afdc20fe1a2128e8db
ThanhLuyen2003 <145136443+ThanhLuyen2003@users.noreply.github.com>
2023-12-07 00:24:10 +0700

    Update odercontroller.js

--- controllers/odercontroller.js
+++ controllers/odercontroller.js
@@ -1,62 +1,34 @@
 var myMD = require('../models/model');
-var fs = require ('fs');
+var fs = require('fs');
 
-exports.list = async (req,res,next) =>{
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 5;
-    const skip = (page - 1) * limit;
-    const sortBy = req.query.sortBy || 'nameU';
-    const sortOrder = req.query.sortOrder || 'asc';
-    var dieu_kien_loc = null;
-    if(typeof(req.query.billSearch) !== 'undefined'){
-        dieu_kien_loc = { nameU: { $regex: new RegExp(req.query.billSearch, 'i') } };
-    }
-    try {
-        const sortOptions = {};
-        sortOptions[sortBy] = sortOrder === 'desc' ? -1 : 1;
-        var listOders = await myMD.OrderModel.find(dieu_kien_loc).skip(skip).limit(limit).sort(sortOptions);
-        var totalOders = await myMD.OrderModel.countDocuments(); 
-      } catch (err) {
-        console.error('Error retrieving users:', err);
-        res.status(500).json({ error: 'Internal server error' });
-      }
-    res.render('oder/oder',{listOders: listOders,
-                            currentPage: page,
-                            totalPages: Math.ceil(totalOders / limit),
-                            totalOders});
+exports.list = async (req, res, next) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 5;
+  const skip = (page - 1) * limit;
+  const sortBy = req.query.sortBy || 'nameU';
+  const sortOrder = req.query.sortOrder || 'asc';
+  var dieu_kien_loc = null;
+  if (typeof (req.query.billSearch) !== 'undefined') {
+    dieu_kien_loc = { nameU: { $regex: new RegExp(req.query.billSearch, 'i') } };
+  }
+  try {
+    const sortOptions = {};
+    sortOptions[sortBy] = sortOrder === 'desc' ? -1 : 1;
+    var listOders = await myMD.OrderModel.find(dieu_kien_loc).skip(skip).limit(limit).sort(sortOptions);
+    var totalOders = await myMD.OrderModel.countDocuments();
+  } catch (err) {
+    console.error('Error retrieving users:', err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+  res.render('oder/oder', {
+    listOders: listOders,
+    currentPage: page,
+    totalPages: Math.ceil(totalOders / limit),
+    totalOders
+  });
 }
 
-exports.duyetSP = async (req,res,next) =>{
-    console.log('Xác nhận đơn hàng');
-    let ids = req.params.ids;
-
-    console.log(ids + 'aaaaaaa');
-    let objBill = await myMD.OrderModel.findById(ids);
-  
-    console.log('BEFORE' + objBill.status);
-    let billStatus = objBill.status;
-  
-    if (billStatus === 'Có đơn') {
-      objBill.status = 'Đang giao hàng' ;
-    }
-  
-    if (billStatus === 'Đang giao hàng') {
-      objBill.status = 'Đã giao hàng';
-    }
-  
-    objBill._id = ids;
-  
-    try {
-      console.log('AFTER' + objBill.status);
-      await myMD.OrderModel.findByIdAndUpdate({ _id: ids }, objBill);
-      console.log('AFTER UPDATE' + objBill.status);
-    } catch (error) {
-      console.log(error);
-    }
-  
-    res.redirect('/oder');
-}
-exports.huySP = async (req, res, next) =>{
+exports.duyetSP = async (req, res, next) => {
   console.log('Xác nhận đơn hàng');
   let ids = req.params.ids;
 
@@ -67,7 +39,37 @@ exports.huySP = async (req, res, next) =>{
   let billStatus = objBill.status;
 
   if (billStatus === 'Có đơn') {
-    objBill.status = 'Hủy đơn' ;
+    objBill.status = 'Đang giao hàng';
+  }
+
+  if (billStatus === 'Đang giao hàng') {
+    objBill.status = 'Đã giao hàng';
+  }
+
+  objBill._id = ids;
+
+  try {
+    console.log('AFTER' + objBill.status);
+    await myMD.OrderModel.findByIdAndUpdate({ _id: ids }, objBill);
+    console.log('AFTER UPDATE' + objBill.status);
+  } catch (error) {
+    console.log(error);
+  }
+
+  res.redirect('/oder');
+}
+exports.huySP = async (req, res, next) => {
+  console.log('Xác nhận đơn hàng');
+  let ids = req.params.ids;
+
+  console.log(ids + 'aaaaaaa');
+  let objBill = await myMD.OrderModel.findById(ids);
+
+  console.log('BEFORE' + objBill.status);
+  let billStatus = objBill.status;
+
+  if (billStatus === 'Có đơn') {
+    objBill.status = 'Hủy đơn';
   }
 
   if (billStatus === 'Đang giao hàng') {
@@ -93,7 +95,7 @@ exports.renderPage = async (req, res, next) => {
   try {
     const listOders = await myMD.OrderModel.find();
     res.locals.listOders = listOders;
-    
+
     res.render('oder/oder');
   } catch (err) {
     console.error('Error retrieving data from MongoDB:', err);
