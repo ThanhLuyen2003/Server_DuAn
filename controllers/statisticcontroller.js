ecb451f93a55036d03a3cefc73ed6078a11d7981
tronghung31 <nth31012003@gmail.com>
2023-12-14 23:47:07 +0700

    thong ke tien

--- controllers/statisticcontroller.js
+++ controllers/statisticcontroller.js
@@ -3,60 +3,84 @@ var billMd = require('../models/BillModel');
 const moment = require('moment');
 
 exports.thongkebanhang = async (req, res, next) => {
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
-    const sortBy = req.query.sortBy || 'name';
-    const sortOrder = req.query.sortOrder || 'asc';
-    // tìm kiếm
-    var thong_bao = null;
-    var dieu_kien_loc = null;
-    if (typeof req.query.billSearch !== 'undefined' && req.query.billSearch.trim() !== '') {
-      // Tìm kiếm theo cột 'name'
-      dieu_kien_loc = { 
-          $or: [
-              { name: { $regex: new RegExp(req.query.billSearch, 'i') } },
-              { soluongnhap: parseFloat(req.query.billSearch) || 0 },
-              { price: { $regex: new RegExp(req.query.billSearch, 'i') } },
-              { pricenhap: { $regex: new RegExp(req.query.billSearch, 'i') } }
-          ]
-      };
-    } else {
-      thong_bao = "Không có dữ liệu";
-    }
-    
-    try {
-      const sortOptions = {};
-      sortOptions[sortBy] = sortOrder === 'desc' ? -1 : 1;
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const sortBy = req.query.sortBy || 'name';
+  const sortOrder = req.query.sortOrder || 'asc';
+  // tìm kiếm
+  var thong_bao = null;
+  var dieu_kien_loc = null;
+  if (typeof req.query.billSearch !== 'undefined' && req.query.billSearch.trim() !== '') {
+    // Tìm kiếm theo cột 'name'
+    dieu_kien_loc = {
+      $or: [
+        { name: { $regex: new RegExp(req.query.billSearch, 'i') } },
+        { soluongnhap: parseFloat(req.query.billSearch) || 0 },
+        { price: { $regex: new RegExp(req.query.billSearch, 'i') } },
+        { pricenhap: { $regex: new RegExp(req.query.billSearch, 'i') } }
+      ]
+    };
+  } else {
+    thong_bao = "Không có dữ liệu";
+  }
+
+  try {
+    const sortOptions = {};
+    sortOptions[sortBy] = sortOrder === 'desc' ? -1 : 1;
+
+    const listSatistic = await myMD.productModel.find(dieu_kien_loc).sort(sortOptions).skip((page - 1) * limit).limit(limit);
+    const listOders = await myMD.OrderModel.find();
+    const totalSatistic = await myMD.productModel.countDocuments(dieu_kien_loc);
+
+    // tính tổng tiền nhập
+    let total = 0;
+    const allListSatistic = await myMD.productModel.find(dieu_kien_loc).sort(sortOptions);
+    allListSatistic.forEach((row) => {
+      const price = row.pricenhap;
+      const slnhap = row.soluongnhap;
+      const tongtiennhap = price * slnhap;
+      total += tongtiennhap;
+    });
+
+    // Assuming you have a product model and an order model
+
+    // Calculate total sales value
+    let totalSales = 0;
+    listOders.forEach((order) => {
+      if (order.status === 'Đã giao hàng') {
+        order.products.forEach((product) => {
+          // Replace these field names with the actual field names from your data structure
+          const priceban = product.price;
+          const slban = parseInt(product.quantity || 0, 10);
+          const tongtienban = priceban * slban;
+          totalSales += tongtienban;
+        });
+      }
+    });
+      
+
+   
+    console.log('Tổng tiền bán hàng:', totalSales);
+
+
   
-      const listSatistic = await myMD.productModel.find(dieu_kien_loc).sort(sortOptions).skip((page - 1) * limit).limit(limit);
-      const listOders = await myMD.OrderModel.find();
-      const totalSatistic = await myMD.productModel.countDocuments(dieu_kien_loc);
-  
-      // tính tổng tiền nhập
-      let total = 0;
-      const allListSatistic = await myMD.productModel.find(dieu_kien_loc).sort(sortOptions);
-      allListSatistic.forEach((row) => {
-        const price = row.pricenhap;
-        const slnhap = row.soluongnhap;
-        const tongtiennhap = price * slnhap;
-        total += tongtiennhap;
-      });
-  
-      const totalPages = Math.ceil(totalSatistic / limit);
-  
-      res.render('thongke/thongkebanhang', {
-        listSatistic: listSatistic,
-        listOders: listOders,
-        currentPage: page,
-        totalPages: totalPages,
-        totalSatistic: totalSatistic,
-        total: total
-      });
-  
-    } catch (err) {
-      console.error('Error retrieving users:', err);
-      res.status(500).json({ error: 'Internal server error' });
-    }
+
+    const totalPages = Math.ceil(totalSatistic / limit);
+
+    res.render('thongke/thongkebanhang', {
+      listSatistic: listSatistic,
+      listOders: listOders,
+      currentPage: page,
+      totalPages: totalPages,
+      totalSatistic: totalSatistic,
+      total: total,
+      totalSales: totalSales,
+    });
+
+  } catch (err) {
+    console.error('Error retrieving users:', err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 }
 
 // Hưng 
