c387ec6ccb8fd674575f923f2258cf321b8222f2
ThanhLuyen2003 <145136443+ThanhLuyen2003@users.noreply.github.com>
2023-12-17 13:54:41 +0700

    socket

--- app.js
+++ app.js
@@ -78,4 +78,6 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
+
+
 module.exports = app;
--- routes/oder.route.js
+++ routes/oder.route.js
@@ -3,10 +3,10 @@ var router = express.Router();
 var check_login = require('../middlewares/check_login');
 var oderController = require('../controllers/odercontroller');
 
-router.get('/',check_login.yeu_cau_dang_nhap, oderController.list);
+router.get('/', check_login.yeu_cau_dang_nhap, oderController.list);
 
 console.log('Before defining route for /oder/filter_data');
-router.get('/filter_data',check_login.yeu_cau_dang_nhap, oderController.OderFilter);
+router.get('/filter_data', check_login.yeu_cau_dang_nhap, oderController.OderFilter);
 
 router.get('/duyetoders/:ids', check_login.yeu_cau_dang_nhap, oderController.duyetSP);
 
